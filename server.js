cb922bf37528844a87f6b69ea5935fb7d42f5ebb
Alex Horton <alexhortonmusic@gmail.com>
2016-10-04 15:36:26 -0500

    game logic on front end

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const express = require('express')
 const { Server } = require('http')
@@ -10,7 +10,7 @@ const server = Server(app)
 const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
-const MONGODB_URL = process.env.MONGODB_URL || 'mognodb://localhost:27017/tickytacky'
+const MONGODB_URL =  process.env.MONGODB_URL || 'mongodb://localhost:27017/tickietackietoe'
 
 app.set('view engine', 'pug')
 
@@ -20,4 +20,9 @@ app.get('/', (req, res) => res.render('index'))
 
 mongoose.connect(MONGODB_URL, () => {
   server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+})
+
+io.on('connect', socket => {
+  console.log(`Socket connected: ${socket.id}`)
+  socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
 })
